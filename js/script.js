f8a3d0855b6aeaf0b22709585efc295c97ca4b0f
PRAFFUL RANJAN <40630844+prafful360@users.noreply.github.com>
2019-01-30 16:08:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
-// let about = document.getElementById("about");
-// about.addEventListener("click", about1);
 
-// function about1() {
-//   alert("you don't wanna know");
-// }
-
-// const dialogue = document.getElementById("dialogue");
-
-// let details = document.getElementById("profiler");
-// details.addEventListener("click", e => {
-//   console.log(dialogue);
-//   dialogue.classList.add("show-dialogue");
-//   dialogue.classList.remove("dialogue");
-// });
 let boxAnime = document.getElementById("back");
 boxAnime.addEventListener("click", anime1);
 
@@ -22,19 +8,29 @@ function anime1() {
   const resume = document.getElementById("resume");
   const body1 = document.getElementsByTagName("BODY")[0];
   const newaboutMe = document.getElementById("detailedBox");
+  const newSkills = document.getElementById("skillsPara");
   // Styles
   body1.style.backgroundColor = "#66BB6A";
   newContainer.style.display = "block";
   oldContainer.style.display = "none";
   newContainer.style.backgroundColor = "#FAFAFA";
   newaboutMe.style.display = "none";
+  newSkills.style.display = "none";
 
-  // ABout work
+  // ABout me
   let about1 = document.getElementById("aboutLi");
   about1.addEventListener("click", aboutMe);
 
   function aboutMe() {
     const newaboutMe = document.getElementById("detailedBox");
     newaboutMe.style.display = "block";
+  }
+  // Skills
+  let skills1 = document.getElementById("skillsLi");
+  skills1.addEventListener("click", aboutSkills);
+  function aboutSkills() {
+    const newSkills = document.getElementById("skillsPara");
+    newSkills.style.display = "block";
+    newaboutMe.style.display = "none";
   }
 }
